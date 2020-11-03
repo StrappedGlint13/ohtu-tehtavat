a701a642f24f51550bce1cf4aadf36d1cf3e1efa
StrappedGlint13 <matiasbrax@gmail.com>
2020-11-03 15:59:55 +0200

    10-12 done

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,20 +1,15 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Viitegeneraattori implements ViitegeneraattoriInterface {
-
-    private static Viitegeneraattori instanssi;
-
-    public static Viitegeneraattori getInstance() {
-        if (instanssi == null) {
-            instanssi = new Viitegeneraattori();
-        }
-
-        return instanssi;
-    }
-    
+ 
     private int seuraava;
     
-    private Viitegeneraattori(){
+    @Autowired
+    public Viitegeneraattori(){
         seuraava = 1;    
     }
     
