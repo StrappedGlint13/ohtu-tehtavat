3547971a5ba01f054d4a086be331a7f4d6c8b10b
StrappedGlint13 <matiasbrax@gmail.com>
2020-11-11 11:24:00 +0200

    new feature with steps added

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import java.util.Random;
 import static org.junit.Assert.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,10 +24,47 @@ public class Stepdefs {
         element.click();   
     }    
     
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();   
+    }
+    
+    @When("too short username {string} and valid password {string} is given")
+    public void tooShortUsername(String username, String password){
+        createNewUser(username, password);
+    }
+    
+    @When("correct username {string} and password {string} confirmations fails")
+    public void correctUserNameFailedPasswordConfirmation(String username, String password){
+        failedRegistered(username, password);
+    }
+    
+    @When("correct username {string} and too short password {string} is given")
+    public void correctUsernameTooShortPassword(String username, String password){
+        createNewUser(username, password);
+    }
+    
+    @Then("user is not created and error {string} is reported")
+    public void UserHasTooShortPassword(String error) {
+        pageHasContent(error);
+    }
+    
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
     }    
+    
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUserNameAndPasswordAreGiven(String username, String password) {
+        createNewUser(username, password);
+    }
+    
+    @Then("a new user is created")
+    public void newUserIsLoggedIn() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
     
     @Then("user is logged in")
     public void userIsLoggedIn() {
@@ -80,4 +118,28 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+    
+    private void createNewUser(String username, String password) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+    }
+    
+    private void failedRegistered (String username, String password) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("vaaraconffa");
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+    }
 }
