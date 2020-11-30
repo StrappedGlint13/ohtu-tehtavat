4314c1b5d278c3d69c049a703c0819373edef43b
StrappedGlint13 <matiasbrax@gmail.com>
2020-11-30 10:05:54 +0200

    OR-operation

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -8,6 +8,17 @@ public class Main {
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
           
-        System.out.println(stats.matches(new All()).size());
+        Matcher m = new And(
+                new HasAtLeast(50, "points"),
+                new Or( 
+                    new PlaysIn("NYR"),
+                    new PlaysIn("NYI"),
+                    new PlaysIn("BOS")
+                )
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
     }
 }
